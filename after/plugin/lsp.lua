efc054ab19cf2e860e011b94e6a8a685b1eba9dd
inscrow <rickyrie98ma@gmail.com>
2023-10-26 14:26:14 +0200

    c-y selects first option in cmp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -40,7 +40,7 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
 
     -- Ctrl+Space to trigger completion menu
     ['<C-Space>'] = cmp.mapping.complete(),
