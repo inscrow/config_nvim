6824f8a32956128dcb1ae42e90c672e8f372f9db
inscrow <rickyrie98ma@gmail.com>
2023-11-07 22:25:43 +0100

    added overlength plugin

---
+++ after/plugin/overlength.lua
@@ -0,0 +1,8 @@
+local overlength = require('overlength')
+overlength.setup({
+    colors = {
+        bg = '#592929',
+    },
+    default_overlength = 80,
+})
+overlength.set_overlength("java", 100)
