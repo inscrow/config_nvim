1a12fbb5aeed1e86cd50f50dcacf4bcbd7cf0285
inscrow <rickyrie98ma@gmail.com>
2023-11-11 23:31:48 +0100

    changed some settings

--- lua/rie/settings.lua
+++ lua/rie/settings.lua
@@ -9,3 +9,22 @@ vim.o.mouse = false
 -- numbers
 vim.o.number = true
 vim.o.relativenumber = true
+
+-- block cursor
+vim.o.guicursor = ""
+
+-- no soft wrapping
+vim.o.wrap = false
+
+-- search
+vim.o.hlsearch = false
+vim.o.incsearch = true
+
+-- 2 space indentation in java code
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "java",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+	end
+})
