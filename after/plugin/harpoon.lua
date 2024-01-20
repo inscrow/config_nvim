073def55dbc17468f05ffd8f869d259970b26780
inscrow <rickyrie98ma@gmail.com>
2024-01-20 17:37:31 +0100

    Let harpoon open tmux or nvim terminal

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,8 +1,15 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+local tmux = require("harpoon.tmux")
 local term = require("harpoon.term")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<leader>t", function() term.gotoTerminal(1) end)
+vim.keymap.set("n", "<leader>t", function()
+    if vim.env.TMUX == nil then
+        term.gotoTerminal(1)
+    else
+        tmux.gotoTerminal(1)
+    end
+end)
 vim.keymap.set("t", "<esc>", "<c-\\><c-n>")
